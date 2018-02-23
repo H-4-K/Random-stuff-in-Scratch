b1982e4fca1a7fd7cc271c667741c2b21a3bd8e2
H-4-K <34457885+H-4-K@users.noreply.github.com>
2018-02-23 23:45:53 +0000

    Update randomad.js

--- randomad.js
+++ randomad.js
@@ -1,6 +1,6 @@
 var x=document.getElementById("ads");
 var adList=[
-{"https://scratch.mit.edu/projects/192395027/","https://1.bp.blogspot.com/-wmGMfHdls68/WpCjhoJCtBI/AAAAAAAAACA/73PQLWfcl6gDg0JMDfhsrFhrPP340nAswCPcBGAYYCw/s1600/Ad1.png"},
+["https://scratch.mit.edu/projects/192395027/","https://1.bp.blogspot.com/-wmGMfHdls68/WpCjhoJCtBI/AAAAAAAAACA/73PQLWfcl6gDg0JMDfhsrFhrPP340nAswCPcBGAYYCw/s1600/Ad1.png"],
 ];
 var rand=adList[Math.round(adList.length*Math.random())-1];
 x.children[0].src=rand[1];

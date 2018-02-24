ef2479a4f71a012e13e0d0b5bafc2f12c057c838
H-4-K <34457885+H-4-K@users.noreply.github.com>
2018-02-24 11:19:14 +0000

    Update randomad.js

--- randomad.js
+++ randomad.js
@@ -1,4 +1,3 @@
-var canRunAds = true;
 var x=document.getElementById("ads");
 var adList=[
 ["https://scratch.mit.edu/projects/192395027/","https://1.bp.blogspot.com/-wmGMfHdls68/WpCjhoJCtBI/AAAAAAAAACA/73PQLWfcl6gDg0JMDfhsrFhrPP340nAswCPcBGAYYCw/s1600/Ad1.png"],

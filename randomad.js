e95eea31f1350afbf8efaf775aced27065b84ac8
H-4-K <34457885+H-4-K@users.noreply.github.com>
2018-02-25 11:47:15 +0000

    Update randomad.js

--- randomad.js
+++ randomad.js
@@ -1,8 +1,18 @@
 var x=document.getElementById("ads");
 var adList=[
 ["https://scratch.mit.edu/projects/192395027/","https://1.bp.blogspot.com/-wmGMfHdls68/WpCjhoJCtBI/AAAAAAAAACA/73PQLWfcl6gDg0JMDfhsrFhrPP340nAswCPcBGAYYCw/s1600/Ad1.png"],
-["https://scratch.mit.edu/projects/200514383/","https://lh6.googleusercontent.com/G_M0IzLoNIP28veWlJznE9CcihL5MAeEu6EWnBuoyROQHxv5WZWW5LsAWMFS7E2W5BokdFBIAuYIE1GveXd4=w1366-h637"],
+["https://scratch.mit.edu/projects/200514383/","https://1.bp.blogspot.com/-J509tZxUKqc/WpKNUwrsv7I/AAAAAAAAACk/M6bDlG202FcxqMpvqZ-rgW3PfZAjw86FwCPcBGAYYCw/s1600/Ad2.png"],
+];
+var adList2=[
+["https://scratch.mit.edu/projects/205600380/","https://4.bp.blogspot.com/-6YI-KepAwYw/WpKOF2ihlMI/AAAAAAAAACw/qODoQecIOPkjySTBIldsNH8KX5EHMP7mQCPcBGAYYCw/s1600/Ad2-1.png"],
+["https://scratch.mit.edu/projects/190480002/","https://uploads.scratch.mit.edu/projects/thumbnails/190480002.png"]
 ];
 var rand=adList[Math.round((adList.length-1)*Math.random())];
 x.children[0].src=rand[1];
 x.href=rand[0]
+if (document.getElementById("ads2")!=undefined) {
+  x=document.getElementById("ads2");
+  rand=adList2[Math.round((adList2.length-1)*Math.random())];
+x.children[0].src=rand[1];
+x.href=rand[0]
+}
